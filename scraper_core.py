cbd12201a991a4df3cc549a0f48f543859b16334
CarlosTjoeNij <46446937+CarlosTjoeNij@users.noreply.github.com>
2025-09-16 16:11:47 +0200

    Update scraper_core.py

--- scraper_core.py
+++ scraper_core.py
@@ -9,7 +9,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import streamlit as st  # Voor secrets
 
 from platformen.striive import scrape_striive
 from platformen.flextender import scrape_flextender
