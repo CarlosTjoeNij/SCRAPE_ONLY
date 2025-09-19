e7ac157290b99fc332998594136aa08340474556
CarlosTjoeNij <46446937+CarlosTjoeNij@users.noreply.github.com>
2025-09-19 09:26:47 +0200

    Update yacht.py

--- platformen/yacht.py
+++ platformen/yacht.py
@@ -44,6 +44,7 @@ def get_chrome_driver(timeout=15):
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--window-size=1920x1080")
+    chrome_options.add_argument("--remote-debugging-port=9222")  # voorkomt crashes
 
     driver = webdriver.Chrome(options=chrome_options)  # Selenium Manager regelt driver
     driver.implicitly_wait(timeout)
