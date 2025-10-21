cbb8eea794a764f8ea3099233fa5706121972dc6
CarlosTjoeNij <46446937+CarlosTjoeNij@users.noreply.github.com>
2025-10-21 14:09:00 +0200

    Add scraping for werkeninnoordoostbrabant

--- scraper_core.py
+++ scraper_core.py
@@ -16,56 +16,57 @@ from platformen.yacht import scrape_yacht
 from platformen.circle8 import scrape_circle8
 from platformen.igom import scrape_igom
 from platformen.werkenvoornederland import scrape_werkenvoornederland
+from platformen.werkeninnoordoostbrabant import scrape_werkeninnoordoostbrabant
 
 # --- COMBINED SCRAPE ---
 def scrape_all_jobs():
     start_time = time.time()
     dfs = []
 
-    # Striive
-    try:
-        print("➡️ Start scrape: Striive")
-        df_striive = scrape_striive()
-        dfs.append(df_striive)
-        print(f"✅ Striive done, {len(df_striive)} rows")
-    except Exception as e:
-        print(f"❌ Fout tijdens scraping Striive: {e}")
+    # # Striive
+    # try:
+    #     print("➡️ Start scrape: Striive")
+    #     df_striive = scrape_striive()
+    #     dfs.append(df_striive)
+    #     print(f"✅ Striive done, {len(df_striive)} rows")
+    # except Exception as e:
+    #     print(f"❌ Fout tijdens scraping Striive: {e}")
 
-    # Flextender
-    try:
-        print("➡️ Start scrape: Flextender")
-        df_flex = scrape_flextender()
-        dfs.append(df_flex)
-        print(f"✅ Flextender done, {len(df_flex)} rows")
-    except Exception as e:
-        print(f"❌ Fout tijdens scraping Flextender: {e}")
+    # # Flextender
+    # try:
+    #     print("➡️ Start scrape: Flextender")
+    #     df_flex = scrape_flextender()
+    #     dfs.append(df_flex)
+    #     print(f"✅ Flextender done, {len(df_flex)} rows")
+    # except Exception as e:
+    #     print(f"❌ Fout tijdens scraping Flextender: {e}")
 
-    # Yacht
-    try:
-        print("➡️ Start scrape: Yacht")
-        df_yacht = scrape_yacht()
-        dfs.append(df_yacht)
-        print(f"✅ Yacht done, {len(df_yacht)} rows")
-    except Exception as e:
-        print(f"❌ Fout tijdens scraping Yacht: {e}")
+    # # Yacht
+    # try:
+    #     print("➡️ Start scrape: Yacht")
+    #     df_yacht = scrape_yacht()
+    #     dfs.append(df_yacht)
+    #     print(f"✅ Yacht done, {len(df_yacht)} rows")
+    # except Exception as e:
+    #     print(f"❌ Fout tijdens scraping Yacht: {e}")
 
-    # Circle8
-    try:
-        print("➡️ Start scrape: Circle8")
-        df_circle8 = scrape_circle8()
-        dfs.append(df_circle8)
-        print(f"✅ Circle8 done, {len(df_circle8)} rows")
-    except Exception as e:
-        print(f"❌ Fout tijdens scraping Circle8: {e}")
+    # # Circle8
+    # try:
+    #     print("➡️ Start scrape: Circle8")
+    #     df_circle8 = scrape_circle8()
+    #     dfs.append(df_circle8)
+    #     print(f"✅ Circle8 done, {len(df_circle8)} rows")
+    # except Exception as e:
+    #     print(f"❌ Fout tijdens scraping Circle8: {e}")
 
-    # IGOM
-    try:
-        print("➡️ Start scrape: IGOM")
-        df_igom = scrape_igom()
-        dfs.append(df_igom)
-        print(f"✅ IGOM done, {len(df_igom)} rows")
-    except Exception as e:
-        print(f"❌ Fout tijdens scraping IGOM: {e}")
+    # # IGOM
+    # try:
+    #     print("➡️ Start scrape: IGOM")
+    #     df_igom = scrape_igom()
+    #     dfs.append(df_igom)
+    #     print(f"✅ IGOM done, {len(df_igom)} rows")
+    # except Exception as e:
+    #     print(f"❌ Fout tijdens scraping IGOM: {e}")
 
     # werkenvoornederland
     try:
@@ -75,6 +76,14 @@ def scrape_all_jobs():
         print(f"✅ werkenvoornederland done, {len(df_werkenvoornederland)} rows")
     except Exception as e:
         print(f"❌ Fout tijdens scraping werkenvoornederland: {e}")
+
+    try:
+        print("➡️ Start scrape: werkeninnoordoostbrabant.nl")
+        df_werkeninnoordoostbrabant.nl = scrape_werkeninnoordoostbrabant.nl()
+        dfs.append(df_werkeninnoordoostbrabant.nl)
+        print(f"✅ werkeninnoordoostbrabant.nl done, {len(df_werkeninnoordoostbrabant.nl)} rows")
+    except Exception as e:
+        print(f"❌ Fout tijdens scraping werkeninnoordoostbrabant.nl: {e}")
 
     if dfs:
         df_combined = pd.concat(dfs, ignore_index=True)
