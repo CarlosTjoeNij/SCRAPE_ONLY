190648aa38e09f82f1041a8d64b68b62f7efd851
CarlosTjoeNij <46446937+CarlosTjoeNij@users.noreply.github.com>
2025-11-18 09:35:23 +0100

    Update daily_scraper.py

--- daily_scraper.py
+++ daily_scraper.py
@@ -9,7 +9,7 @@ BUCKET_NAME = "scrapes_cvmatcher"
 
 def upload_to_gcs(df: pd.DataFrame):
     """Upload DataFrame naar GCS als Parquet-bestand."""
-    today_str = datetime.date.today().strftime("%d%m%Y")
+    today_str = datetime.date.today().strftime("%Y-%m-%d")
     filename = f"jobs_{today_str}.parquet"
 
     client = storage.Client()
