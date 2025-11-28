b4867f388f4c9b3e7d29f6c97a745f29a6223162
CarlosTjoeNij <46446937+CarlosTjoeNij@users.noreply.github.com>
2025-11-28 16:01:11 +0100

    Update scraper_core.py

--- scraper_core.py
+++ scraper_core.py
@@ -9,6 +9,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import threading
 
 from platformen.striive import scrape_striive
 from platformen.flextender import scrape_flextender
